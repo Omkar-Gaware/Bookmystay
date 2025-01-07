abc2bf43bb96c125fc641a79dff90086b7ead9d9
Omkar-Gaware <omkargaware09@gmail.com>
2025-01-07 23:12:30 +0530

    Created Login/SignUp Page And Authonticate By Using Passport

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const app = express();
 const mongoose = require("mongoose");
 const Mongo_Url = "mongodb://127.0.0.1:27017/wanderlust";
 const path = require("path");
+const wrapAsync = require("./util/wrapAsync.js");
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
 const ExpressError = require("./util/ExpressError.js");
-const listings = require("./routes/listing.js");
-const reviews = require("./models/reviews.js");
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./models/reviews.js");
+const userRouter = require("./models/user.js");
 const session = require("express-session");
 const flash = require("connect-flash");
-
+const passport = require("passport");
+const Localpassport = require("passport-local");
+const User = require("./models/user.js");
 
 main()
     .then(() => {
@@ -31,41 +35,87 @@ app.engine('ejs', ejsmate);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-const sessionOptions ={
-    secret : "mysupersecretcode",
-    resave : false,
-    saveUninitialized : true,
-    cookie:{
-        expires : Date.now() + 7*24*60*60*1000,
-        maxAge : 7*24*60*60*1000,
-        httpOnly : true,
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     }
 };
 
 app.use(session(sessionOptions));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Localpassport(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", (req, res) => {
     res.redirect("/listings");
 })
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
 
+app.use("/demoUser", async (req, res) => {
+    let fakeUser = new User({
+        email: "omkargavare100@gmail.com",
+        username: "Omkar_09"
+    })
 
-app.all("*",(req,res,next)=>{
+    let registerUser = await User.register(fakeUser, "Omkar@121");
+    res.send(registerUser);
+})
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/listings", userRouter);
+
+// SignUp
+app.get("/signup", (req, res) => {
+    res.render("Users/signup.ejs")
+})
+
+app.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registerUser = await User.register(newUser, password);
+        console.log(registerUser);
+        req.flash("success", "User was register Sucessfully");
+        res.redirect("/listings");
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup");
+    }
+}))
+
+//Login
+app.get("/login",(req,res)=>{
+    res.render("Users/login.ejs")
+})
+app.post("/login",passport.authenticate("local",
+    {failureRedirect:'/login', failureFlash:true}),
+     async (req,res)=>{
+        req.flash("success","Welcome Back to BookMyStay");
+        res.redirect("/listings")
+})
+app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 })
-app.use((err,req,res,next)=>{
-    let {statuscode = 500, message = "Somthing Went Wrong!"} = err;
-    
-    res.render("listings/error.ejs",{err});
+app.use((err, req, res, next) => {
+    let { statuscode = 500, message = "Somthing Went Wrong!" } = err;
+
+    res.render("listings/error.ejs", { err });
 })
 app.listen(8080, () => {
     console.log("server listening on port 8080.")
